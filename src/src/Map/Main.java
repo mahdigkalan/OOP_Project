a5613de8474e630ab93b9c6eecb4e8f9bd699290
growing mistake <113902138+100tari@users.noreply.github.com>
2023-06-26 07:09:53 -0700

    Update Main.java

--- src/src/Map/Main.java
+++ src/src/Map/Main.java
@@ -1,35 +1,14 @@
 package Map;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Graph<Long> graph = Graph.readGraph("D:\\University\\term 2\\OOP\\OOP_Project\\assets\\graph.txt");
-        Dijkstra<Long> dijkstra = new Dijkstra<>(graph);
+    public static void main(String[] args) {
+        String mapPath = "";
+        Integer start = null, end = null;
 
-        File file = new File("D:\\University\\term 2\\OOP\\OOP_Project\\assets\\out.txt");
-        FileWriter fileWriter = new FileWriter(file);
-        fileWriter.write("root,target,path,cost\n");
+        Map map = new Map(mapPath);
 
-        for(Long root = 1L ; root <= 100 ; root++) {
-            for(Long target = root + 1; target <= 100 ; target++) {
-                System.out.println("searching for root = "+root+" and target = "+target);
-                fileWriter.write(String.valueOf(root)+","+String.valueOf(target)+",");
-                if(dijkstra.search(root, target)) {
-                    System.out.println("path found: "+dijkstra.getPath());
-                    System.out.println("cost: "+dijkstra.getCost());
-                    fileWriter.write(dijkstra.getPath().toString().replaceAll(","," <-").replaceAll("\\]","").replaceAll("\\[", "")+
-                    ","+dijkstra.getCost().toString());
-                } else {
-                    System.out.println("there is no path");
-                    fileWriter.write("null,null");
-                }
-                System.out.println("-----------------------------------------------------");
-                fileWriter.write("\n");
-            }
-        }
-        fileWriter.close();
+        System.out.println(map.getCost(start, end));
+        System.out.println(map.getPath(start, end));
+        System.out.println(map.getTime(start, end)); // in seconds
     }
 }
