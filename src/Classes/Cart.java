3c6914bfd823450fb241addbcc405ee78cfa22d0
MAHDI <mahdigholizadek@gmail.com>
2023-07-01 16:58:48 +0430

    last changes

--- src/Classes/Cart.java
+++
@@ -1,8 +0,0 @@
-package Classes;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-
-public class Cart implements Serializable {
-    public ArrayList<Order> cartorders = new ArrayList<Order>();
-}
